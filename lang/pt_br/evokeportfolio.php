1a45fd2979fb53cfa653e02dda736adbdd30cde4
Willian Mano <willianmanoaraujo@gmail.com>
2021-08-15 21:13:33 -0300

    Closes #21 - Create portfolio Portuguese strings

---
+++ lang/pt_br/evokeportfolio.php
@@ -0,0 +1,113 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_evokeportfolio
+ * @category    string
+ * @copyright   2021 Willian Mano <willianmanoaraujo@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['evokeportfolio:addinstance'] = 'Adicionar um portfolio';
+$string['evokeportfolio:view'] = 'Visualizar portfolio';
+$string['missingidandcmid'] = 'Faltando o ID do módulo do curso';
+$string['modulename'] = 'Portfolio';
+$string['modulename_help'] = 'Use o módulo portfolio para gerenciar as criações dos usuários...';
+$string['modulenameplural'] = 'Portfolios';
+$string['noportfolioinstances'] = 'Não foram encontradas instâncias de portfolios';
+$string['pluginadministration'] = 'Administração do portfolio';
+$string['pluginname'] = 'Portfolio';
+$string['fieldset'] = 'Portfolio';
+$string['name'] = 'Nome';
+$string['view'] = 'Visualizar';
+$string['eventsubmissionsent'] = 'Envio realizado';
+$string['eventsubmissionupdated'] = 'Envio atualizado';
+$string['gradinglocked'] = 'A avaliação está atualimente bloqueada para esta atividade.';
+$string['viewsubmission'] = 'Visualizar envio';
+$string['notsubmitted'] = 'Não enviado';
+$string['evaluated'] = 'Avaliado';
+
+$string['datelimit'] = 'Data limite';
+$string['datelimit_help'] = 'Após esta data, estudantes não podemo mais realizar envios.';
+$string['groupactivity'] = 'Atividade em grupo';
+$string['individualctivity'] = 'Atividade individual';
+$string['groupgradingmode'] = 'Modo de avaliação do grupo';
+$string['groupgradingmode_help'] = '<b>Avaliação do grupo: </b> todos os usuários do grupo receberão a mesma nota.<br><b>Avaliação individual:</b> os usuários receberão notas individualmente.';
+$string['groupgrading'] = 'Avaliação do grupo';
+$string['individualgrading'] = 'Avaliação individual';
+$string['section'] = 'Seção';
+$string['illegalaccess'] = 'Acesso ilegal';
+$string['attachmentfile'] = 'Arquivo de anexo';
+$string['downloadfile'] = 'Baixar arquivo';
+
+$string['page_view_gradingsummary'] = 'Resumo da avaliação';
+$string['page_view_hidden'] = 'Oculto para estudantes';
+$string['page_view_participants'] = 'Participantes';
+$string['page_view_timeremaining'] = 'Tempo restante';
+$string['page_view_viewallsubmissions'] = 'Visualizar todos os envios';
+
+$string['page_view_submission_status'] = 'Statuso do envio';
+$string['page_view_addsubmission'] = 'Adicionar envio';
+$string['page_view_editsubmission'] = 'Editar envio';
+$string['page_view_youhavenotsent'] = 'Você ainda não fez um envio.';
+$string['page_view_submissions'] = 'Visualizar envios';
+$string['page_view_usernotingroup_title'] = 'Desculpe!';
+$string['page_view_usernotingroup_text'] = 'Você precisa fazer parte de um grupo para para realizar esta atividade.';
+
+$string['page_submit_comment'] = 'Seu comentário';
+$string['page_submit_attachments'] = 'Anexos';
+
+$string['page_submissions_portfoliocomment'] = 'Mensagem';
+$string['page_submissions_portfoliomentorcomment'] = 'Mensagem to mentor';
+$string['page_submissions_portfolioattachment'] = 'Anexo';
+
+$string['page_viewsubmission_addcomment'] = 'Adicionar comentário';
+$string['page_viewsubmission_addgrade'] = 'Adicionar nota';
+$string['page_viewsubmission_editgrade'] = 'Editar nota';
+$string['page_entries_title'] = 'Envios do portfolio';
+
+$string['save_comment_success'] = 'Comentário adicionado com sucesso.';
+$string['save_grade_success'] = 'Nota adicionada com sucesso.';
+$string['save_submission_success'] = 'Envio realizado com sucesso.';
+$string['update_submission_success'] = 'Envio atualizado com sucesso';
+
+$string['grade'] = 'Nota';
+$string['grade_help'] = 'DIgite a nota do portfolio do estudante aqui.';
+$string['onlynumbers'] = 'Somente números são aceitos.';
+$string['gradefor'] = 'Nota para: {$a}';
+
+$string['validation:commentrequired'] = 'Comentário é obrigatório';
+$string['validation:commentlen'] = 'Comentário precisa ter pelo menos 10 caracteres';
+$string['validation:graderequired'] = 'Nota é obrigatório';
+$string['validation:commentattachmentsrequired'] = 'Você precisa pelo menos adicionar um comentário ou anexar um arquivo.';
+$string['validation:namelen'] = 'Nome precisa ter pelo menos 3 caracteres';
+
+$string['managesections'] = 'Gerenciar seções';
+$string['createsection'] = 'Criar seção';
+$string['editsection'] = 'Editar seção';
+$string['actions'] = 'Ações';
+$string['deleteitem_confirm_title'] = 'Você tem certeza?';
+$string['deleteitem_confirm_msg'] = 'Uma vez deletado, este item não pode mais ser recuperado!';
+$string['deleteitem_confirm_yes'] = 'Sim, pode deletar!';
+$string['deleteitem_confirm_no'] = 'Cancelar';
+$string['deletesection_success'] = 'Seção deletada com sucesso.';
+$string['createsection_success'] = 'Seção criada com sucesso.';
+$string['editsection_success'] = 'Seção editada com sucesso.';
+$string['deletesection_hassubmissions'] = 'Não é possível deletar esta seção porque ela já possui envios feitos nela.';
