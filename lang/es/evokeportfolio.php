493f1de6c84d6cff6fcda4ed11b16ad40c6a7fce
John Moravec <51122406+educationfutures@users.noreply.github.com>
2021-08-17 15:04:09 -0500

    Update evokeportfolio.php

--- lang/es/evokeportfolio.php
+++ lang/es/evokeportfolio.php
@@ -1,22 +1,15 @@
 <?php
 // This file is part of Moodle - https://moodle.org/
-// Este archivo es parte de Moodle
-
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
-// Debería haber recibido una copia de la Licencia Pública General GNU
-// junto con Moodle. De lo contrario, consulte <https://www.gnu.org/licenses/>.
-
 /**
  * Plugin strings are defined here.
  * Las cadenas de Plugin se definen a continuación.
