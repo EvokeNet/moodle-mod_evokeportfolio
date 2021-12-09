da3d0ff4497a90a2ee8b0b9ceb5e45c5aa8e8da2
John Moravec <51122406+educationfutures@users.noreply.github.com>
2021-12-09 15:06:14 -0600

    Update evokeportfolio.php
    small fixes

--- lang/en/evokeportfolio.php
+++ lang/en/evokeportfolio.php
@@ -33,8 +33,8 @@ $string['evaluated'] = 'Evaluated';
 
 $string['datelimit'] = 'Submission deadline';
 $string['datelimit_help'] = 'After this date, agents cannot submit responses.';
-$string['groupactivity'] = 'Team mission'; // team = group; mission = activity
-$string['individualctivity'] = 'Individual mission';
+$string['groupactivity'] = 'Team activity'; // team = group; mission = activity
+$string['individualctivity'] = 'Individual activity';
 $string['groupgradingmode'] = 'Team grading mode';
 $string['groupgradingmode_help'] = '<b>Team grading: </b> all users in the team will receive the same grade.<br><b>Individual grading:</b> users will receive their grades individually.';
 $string['groupgrading'] = 'Team grading';
@@ -45,7 +45,7 @@ $string['attachmentfile'] = 'Attachment file';
 $string['downloadfile'] = 'Download file';
 
 $string['page_view_gradingsummary'] = 'Grading summary';
-$string['page_view_hidden'] = 'Hidden from agents';
+$string['page_view_hidden'] = 'Hidden from students';
 $string['page_view_participants'] = 'Participants';
 $string['page_view_timeremaining'] = 'Time remaining';
 $string['page_view_viewallsubmissions'] = 'View all submissions';
@@ -58,12 +58,12 @@ $string['page_view_submissions'] = 'View submissions';
 $string['page_view_usernotingroup_title'] = 'Sorry!';
 $string['page_view_usernotingroup_text'] = 'You need to be part of a team to do this mission.';
 
-$string['page_submit_comment'] = 'Your comment';
+$string['page_submit_comment'] = 'Your post';
 $string['page_submit_attachments'] = 'Attachments';
 $string['nohavesectionaccess'] = 'You don\'t have access to that section yet.';
 
 $string['page_submissions_portfoliocomment'] = 'Comment';
-$string['page_submissions_portfoliomentorcomment'] = 'Mentor Comments';
+$string['page_submissions_portfoliomentorcomment'] = 'Mentor comments';
 $string['page_submissions_portfolioattachment'] = 'Attachment';
 
 $string['page_viewsubmission_addcomment'] = 'Add comment';
@@ -81,10 +81,10 @@ $string['grade_help'] = '<p><b>Done: </b>Student meet the required standards for
 $string['onlynumbers'] = 'Only numbers are accepted.';
 $string['gradefor'] = 'Grade for: {$a}';
 
-$string['validation:commentrequired'] = 'A comment is required';
-$string['validation:commentlen'] = 'Comment needs to be at least 10 characters long';
+$string['validation:commentrequired'] = 'Post text is required';
+$string['validation:commentlen'] = 'Post text needs to be at least 10 characters long';
 $string['validation:graderequired'] = 'Grade is required';
-$string['validation:commentattachmentsrequired'] = 'You must add a comment or submit a file.';
+$string['validation:commentattachmentsrequired'] = 'You must add text to your post or submit a file.';
 $string['validation:namelen'] = 'Name needs to contain at least 3 characters';
 
 $string['managesections'] = 'Manage sections';
@@ -92,8 +92,8 @@ $string['createsection'] = 'Create section';
 $string['editsection'] = 'Edit section';
 $string['actions'] = 'Actions';
 $string['deleteitem_confirm_title'] = 'Are you sure?';
-$string['deleteitem_confirm_msg'] = 'Once deleted, this item cannot be recovered!';
-$string['deleteitem_confirm_yes'] = 'Yes, delete it!';
+$string['deleteitem_confirm_msg'] = 'Once deleted, this item cannot be recovered.';
+$string['deleteitem_confirm_yes'] = 'Yes, delete it.';
 $string['deleteitem_confirm_no'] = 'Cancel';
 $string['deletesection_success'] = 'Section successfully deleted.';
 $string['createsection_success'] = 'Section successfully created.';
@@ -105,7 +105,7 @@ $string['evokeportfolio:submit'] = 'Permission to send a portfolio submission';
 
 $string['dependentcoursesections'] = 'Dependent course sections';
 $string['nosectionsavailable'] = 'No sections available';
-$string['nosectionsavailable_desc'] = 'There is no sections available for you in this portfolio yet.';
+$string['nosectionsavailable_desc'] = 'There are no sections available for you in this portfolio yet.';
 
 $string['privacy:metadata:evokeportfolio_submissions'] = 'Information about the user\'s submissions for a given portfolio activity';
 $string['privacy:metadata:evokeportfolio_submissions:sectionid'] = 'The ID of the potfolio\'s section';
@@ -132,9 +132,9 @@ $string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Soci
 $string['status'] = 'Status';
 $string['portfoliochapters'] = 'Portfolio chapters';
 $string['portfoliograding'] = 'Portfolio grading';
-$string['chaptersportfolios'] = 'Chapters\'s portfolios';
+$string['chaptersportfolios'] = 'Portfolios in chapter';
 $string['createchapter'] = 'Create chapter';
-$string['courseportfolios'] = 'Course\'s portfolios';
+$string['courseportfolios'] = 'Portfolios in course';
 $string['deletechapter_success'] = 'Chapter successfully deleted.';
 $string['createchapter_success'] = 'Chapter successfully created.';
 $string['editchapter_success'] = 'Chapter successfully edited.';
